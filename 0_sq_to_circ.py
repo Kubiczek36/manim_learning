0c203d72289d59df8d76fcd901c448adec2441d6
Kubiczek36 <jakub.dokulil@imp.ac.at>
2022-12-27 14:37:46 +0100

    class for basic animations

--- 0_sq_to_circ.py
+++ 0_sq_to_circ.py
@@ -15,3 +15,23 @@ class SquareToCircle(Scene):
         self.play(ReplacementTransform(square, circle))
         self.wait()
         self.embed()
+
+class AnimatingMethods(Scene):
+    def construct(self):
+        grid = OldTex(r"\pi").get_grid(10, 10, height=4)
+        self.add(grid)
+
+        self.play(grid.animate.shift(LEFT))
+        self.wait()
+        self.play(grid.animate.set_color(YELLOW))
+        self.wait()
+        self.play(grid.animate.set_submobject_colors_by_gradient(BLUE, GREEN))
+        self.wait()
+        self.play(grid.animate.set_height(TAU - MED_SMALL_BUFF))
+        self.embed()
+
+class TextExample(Scene):
+    def construct(self):
+        # To run this scene properly, you should have "Consolas" font in your computer
+        # for full usage, you can see https://github.com/3b1b/manim/pull/680
+        text = Text("Here is a text", font="Consolas", font_size=90)
